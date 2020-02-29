a991459613b6a0f28067c91b091387ac272faa89
Bryan Paronto <bparonto@gmail.com>
2020-02-29 15:33:16 -0600

    adds arrays-left-rotation

--- arrays-left-rotation/src/arrays-left-rotation.ts
+++ arrays-left-rotation/src/arrays-left-rotation.ts
@@ -1,3 +1,7 @@
-export default function sum(a: number, b: number): number {
-  return a + b;
+export default function rotLeft(a: number[], d: number): number[] {
+  for (let i = 0; i < d; i++) {
+    a.push(a.shift());
+  }
+
+  return a;
 }
--- arrays-left-rotation/test/arrays-left-rotation.test.ts
+++ arrays-left-rotation/test/arrays-left-rotation.test.ts
@@ -1,5 +1,75 @@
-import sum from '../src/arrays-left-rotation';
+import rotLeft from '../src/arrays-left-rotation';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+let startingArray;
+
+beforeEach(() => {
+  startingArray = [1, 2, 3, 4, 5];
+});
+
+test('Performs 1 left rotations', () => {
+  const rotations = 1;
+  expect(rotLeft(startingArray, rotations)).toEqual([2, 3, 4, 5, 1]);
+});
+
+test('Performs 2 left rotations', () => {
+  const rotations = 2;
+  expect(rotLeft(startingArray, rotations)).toEqual([3, 4, 5, 1, 2]);
+});
+
+test('Performs 3 left rotations', () => {
+  const rotations = 3;
+  expect(rotLeft(startingArray, rotations)).toEqual([4, 5, 1, 2, 3]);
+});
+
+test('Performs 4 left rotations', () => {
+  const rotations = 4;
+  expect(rotLeft(startingArray, rotations)).toEqual([5, 1, 2, 3, 4]);
+});
+
+test('Performs 10 left rotations', () => {
+  startingArray = [
+    41,
+    73,
+    89,
+    7,
+    10,
+    1,
+    59,
+    58,
+    84,
+    77,
+    77,
+    97,
+    58,
+    1,
+    86,
+    58,
+    26,
+    10,
+    86,
+    51
+  ];
+  const rotations = 10;
+  expect(rotLeft(startingArray, rotations)).toEqual([
+    77,
+    97,
+    58,
+    1,
+    86,
+    58,
+    26,
+    10,
+    86,
+    51,
+    41,
+    73,
+    89,
+    7,
+    10,
+    1,
+    59,
+    58,
+    84,
+    77
+  ]);
 });
